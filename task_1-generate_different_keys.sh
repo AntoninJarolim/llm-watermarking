b7aaa064872221ef7f05a5756037464b5a1fadf2
Antonín Jarolím <jarolim.antonin@seznam.cz>
2024-11-27 19:19:50 +0100

    Add script for task 1 generation and detection

---
+++ task_1-generate_different_keys.sh
@@ -0,0 +1,23 @@
+
+conda deactivate && conda activate llm
+# download upload data script
+cp ~/watermark_data/upload_data.sh .
+
+# download input data from merlin
+rsync -rz xjarol06@merlin:/pub/tmp/watermark_data/data/input data --info=progress2 --info=name0
+
+# get current code
+git pull
+
+# Run generation code
+
+for _ in {1..5}; do
+  python generate_texts.py --batch_size 8 --max_length 512 --try_upload --lang english --model_name meta-llama/Llama-3.1-8B --in_data_name data.jsonl --output_path ./data/output/different_keys --class_model_names GumbelNGramWatermarkedLLM
+
+  python generate_texts.py --batch_size 8 --max_length 512 --try_upload --lang english --model_name meta-llama/Llama-3.1-8B --in_data_name squad.jsonl --output_path ./data/output/different_keys_squad --class_model_names GumbelNGramWatermarkedLLM
+done
+
+
+python detecting_watermark.py --data_dir different_keys --model_name meta-llama/Llama-3.1-8B --watermark_name GumbelNGramWatermarkedLLM
+python detecting_watermark.py --data_dir different_keys_squad --model_name meta-llama/Llama-3.1-8B --watermark_name GumbelNGramWatermarkedLLM
+
